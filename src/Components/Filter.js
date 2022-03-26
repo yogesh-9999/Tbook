2e372225c5395c54c14978b0be3ecb239bcd2b64
YOGESH CHAND UPADHYAY <84492118+yogesh-9999@users.noreply.github.com>
2022-03-26 16:23:00 +0530

    basic

---
+++ src/Components/Filter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Style.css'
+export default function Filter() {
+  return (
+    <div className='filter'>
+        
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus dolores, facere ab dolore fuga temporibus! Et, cum quisquam. Soluta deleniti ipsam itaque animi commodi distinctio architecto hic 
+        </p>
+    </div>
+  )
+}
---
+++ src/Components/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Style.css";
+export default function NavBar() {
+  return (
+    <>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">Rooms</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+        <li>
+          <a href="/">Blog</a>
+        </li>
+        <li>
+          <a href="/">Contact Us</a>
+        </li>
+        <li>
+          <a href="/">Book Now</a>
+        </li>
+      </ul>
+    </>
+  );
+}
