c02586aea4faf7150ee828b230d6bbae948fe29d
Your Name <you@example.com>
2022-03-28 13:40:39 +0530

    Commiting changes

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -3,10 +3,7 @@ import './Style.css'
 export default function Filter() {
   return (
     <div className='filter'>
-        
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus dolores, facere ab dolore fuga temporibus! Et, cum quisquam. Soluta deleniti ipsam itaque animi commodi distinctio architecto hic 
-        </p>
+      <p>abssd</p>
     </div>
-  )
+  );
 }
